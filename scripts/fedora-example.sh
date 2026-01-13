d37be355e03eb54beda5b1f432b4088f8a33c66f
UnTamedStArBlOb <50789094+QEU-B-458@users.noreply.github.com>
2026-01-12 20:23:32 -0500

    Add RPM Fusion repository and swap ffmpeg packages

--- scripts/fedora-example.sh
+++ scripts/fedora-example.sh
@@ -6,3 +6,5 @@
 # Update the container and install packages
 dnf update -y
 grep -v '^#' ./fedora-example.packages | xargs dnf install -y
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-(rpm -E %fedora).noarch.rpm
+dnf swap -y ffmpeg-free ffmpeg --allowerasing
